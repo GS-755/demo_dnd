76df6237fd54e67fe24a44ce2a19d3f040b48491
GS-755 <21dh114236@st.huflit.edu.vn>
2024-10-14 01:10:31 +0700

    Add Bootstrap contents

--- src/pages/homepage/App.js
+++ src/pages/homepage/App.js
@@ -1,9 +1,17 @@
 import './App.css';
 
 const App = () => {
+  const message = `Hello, world! ${process.env.API_URL ?? 'undefined'}`; 
+
   return (
     <>
-    
+      <div className='container'> 
+        <div className='row'> 
+          <div className='col'> 
+            <h3 className='m-3'>{message}</h3>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
